215f362263cd0b2ac20a9268e52fac7b4ede02d3
ibrahim ahmed <84781424+ibraimfarag@users.noreply.github.com>
2025-09-13 00:40:35 +0300

    Refactor post card layout for improved responsiveness and consistent styling; adjust margins and classes

--- resources/views/main/posts/includes/posts.blade.php
+++ resources/views/main/posts/includes/posts.blade.php
@@ -1,21 +1,21 @@
-  @foreach ($posts as $post)
-            <div class="col-lg-3 mt-2">
-                <a href="{{ route('the_posts.show', $post->id) }}">
-                    <div class="card box-shadow-medium border-radius-medium card-hover">
-                    <img
-                        src="{{ !empty($post->img) ? $post->img_path : ($post->category->img_path ?? asset('storage/main/categories/default.jpg')) }}"
-                        class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h4 class="h4 card-title text-dark-heading mb-2 line-clamp2">{{ $post->category->name ?? 'غير محدد' }}</h4>
-                        <h3 class="h4 card-text text-dark-content mb-0 line-clamp2">{{ $post->title }}</h3>
-                    </div>
-                    <div class="card-footer bg-transparent">
-                        <h5 class="h4 text-blue-light-heading">المبلغ المطلوب {{ number_format($post->price) }} ريال</h5>
-                    </div>
-                    </div>
-                </a>
-         </div>
-  @endforeach
+@foreach ($posts as $post)
+    <div class="col-lg-3 col-md-6 col-sm-6 mt-2">
+        <a href="{{ route('the_posts.show', $post->id) }}" class="w-100 d-flex">
+            <div class="card box-shadow-medium border-radius-medium card-hover w-100">
+                <img
+                    src="{{ !empty($post->img) ? $post->img_path : ($post->category->img_path ?? asset('storage/main/categories/default.jpg')) }}"
+                    class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="h4 card-title text-dark-heading mb-3 line-clamp2">{{ $post->category->name ?? 'غير محدد' }}</h4>
+                    <h3 class="h4 card-text text-dark-content mb-3 line-clamp2" style="word-wrap: break-word;">{{ $post->title }}</h3>
+                </div>
+                <div class="card-footer bg-transparent price-footer">
+                    <h5 class="h4 text-blue-light-heading mb-0 text-wrap">المبلغ المطلوب {{ number_format($post->price) }} ريال</h5>
+                </div>
+            </div>
+        </a>
+    </div>
+@endforeach
   @if($posts->hasPages())
   <div class="w-100 mt-4">
         <div class="d-flex justify-content-center">
